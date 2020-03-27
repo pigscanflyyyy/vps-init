9d820dc2c76e8d79837dc3571a9289ed79786c5d
ioioioion <xi.nicky@gmail.com>
2020-03-27 16:41:02 +0800

    fix subst problem

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@
 : ${TROJAN_PASSWORD:?is not given.}
 : ${V2RAY_PATH:?is not given.}
 [[ -z $(nslookup $DOMAIN | grep $(curl -s http://checkip.amazonaws.com)) ]] && echo "fail to validate domain" && exit 1
-envsubst '${UUID}' '${V2RAY_PATH}' < config.json.template > ./appdata/etc/v2ray/config.json
+envsubst '${UUID},${V2RAY_PATH}' < config.json.template > ./appdata/etc/v2ray/config.json
 envsubst '${TROJAN_PASSWORD}' < trojan.config.json.template > ./appdata/config/config.json
 envsubst '${DOMAIN}' < docker-compose.yml.template > docker-compose.yml
-envsubst '${DOMAIN}' '${V2RAY_PATH}' < v2ray.subdomain.conf.template > appdata/config/nginx/proxy-confs/v2ray.subdomain.conf
+envsubst '${DOMAIN},${V2RAY_PATH}' < v2ray.subdomain.conf.template > appdata/config/nginx/proxy-confs/v2ray.subdomain.conf
 docker-compose down
 docker-compose pull && docker-compose up -d
 echo finished!
